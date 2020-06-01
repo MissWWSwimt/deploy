fd5377efedaeb934f5cf369df6dee567d9c4d0ae
Lily <misswwswimt@gmail.com>
2020-06-01 20:32:45 +0600

    Updates

---
+++ precache-manifest.055e348f471772abea88d3ebee225072.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79c99ce2276fc3244e646140da171e6",
+    "url": "/%7Bdeploy%7D/index.html"
+  },
+  {
+    "revision": "24a10e1a9ba7da981d66",
+    "url": "/%7Bdeploy%7D/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9806bd3b95173898469",
+    "url": "/%7Bdeploy%7D/static/js/2.79e9ded0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7Bdeploy%7D/static/js/2.79e9ded0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24a10e1a9ba7da981d66",
+    "url": "/%7Bdeploy%7D/static/js/main.47fc4f34.chunk.js"
+  },
+  {
+    "revision": "a006ce2bdcf3e46f97e8",
+    "url": "/%7Bdeploy%7D/static/js/runtime-main.ea6099ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/%7Bdeploy%7D/static/media/logo.5d5d9eef.svg"
+  }
+]);
